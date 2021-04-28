1b3e8692114962839c018a5071786a36f7dfdc86
Song Yang <songyang1218@gmail.com>
2021-04-28 15:36:15 -0400

    Single tokenization

--- tokenize.sh
+++ tokenize.sh
@@ -3,17 +3,23 @@
 # Tokenizes the data set and stores the results in the given directory
 if [ $# -ne 1 ]
 then
-    echo "Usage: ./tokenize.sh where-to-store"
+    echo "Usage: ./tokenize.sh folder-where-to-store #to run the data set"
+    echo "Usage: ./tokenize.sh one-yuml-file #to output a tokenization"
     exit 1
 fi
 
-mkdir -p "$1/test"
-mkdir -p "$1/train"
-mkdir -p "$1/valid"
-for folder in "test" "train" "valid"
-do
-    for file in $folder/*.yuml
+if [ -d "$1" ]
+then
+    mkdir -p "$1/test"
+    mkdir -p "$1/train"
+    mkdir -p "$1/valid"
+    for folder in "test" "train" "valid"
     do
-        ./tokenizer/build/yuml_tokenizer.exe < $file > "$1/$folder/$(basename $file)"
+        for file in $folder/*.yuml
+        do
+            ./tokenizer/build/yuml_tokenizer.exe < $file > "$1/$folder/$(basename $file)"
+        done
     done
-done
+else
+    ./tokenizer/build/yuml_tokenizer.exe < "$1"
+fi
